7b8ac0f00e4fa64e996b8d9814614bb98d3fa2f7
Lukas <pioreklukas@gmail.com>
2025-05-04 12:24:17 +0200

    adden fen index array to uci

--- src/types/engine_state.rs
+++ src/types/engine_state.rs
@@ -38,8 +38,16 @@ impl EngineState {
                     self.board = Board::from_fen(START_POS);
                 }
                 "index" => {
-                    let fens =
-                        ["r3k2r/p1ppqpb1/Bn2pnp1/3PN3/1p2P3/2N2Q2/PPPB1PpP/R3K2R b KQ - 0 1"]; // 0. sebastian lague alpa beta test
+                    let fens = [
+                        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", // 0. Initial Pos
+                        "r3k2r/p1ppqpb1/Bn2pnp1/3PN3/1p2P3/2N2Q2/PPPB1PpP/R3K2R b KQ - 0 1", // 1. sebastian lague alpa beta test
+                        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ", // Pos 2
+                        "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 ", // Pos 3
+                        "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", // Pos 4
+                        "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8 ", // Pos 5
+                        "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ", // Pos 6
+                    ];
+
                     iter.next();
                     if let Some(&idx_str) = iter.next() {
                         match idx_str.parse::<usize>() {
