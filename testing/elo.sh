17aea5f5385e1d9684671742deabea06c8baede6
heofthetea <heofthetea@gmail.com>
2025-10-23 12:52:45 +0200

    play a single game for testing

--- testing/elo.sh
+++ testing/elo.sh
@@ -8,7 +8,7 @@ NEW_VERSION=../target/release/thunfisch
 run_sprt() {
     # todo: name the new engine after the branch or sth
     fastchess \
-        -engine cmd=../target/release/thunfisch name=new -engine cmd=$(PREVIOUS_VERSION) name=old \
+        -engine cmd=../target/release/thunfisch name=new -engine cmd=$PREVIOUS_VERSION name=old \
         -each proto=uci tc=5+0.5 \
         -rounds 20 \
         -repeat \
@@ -16,7 +16,11 @@ run_sprt() {
         -sprt elo0=0 elo1=5 alpha=0.05 beta=0.05
 }
 
-# dummy for testing
-echo "help" | $NEW_VERSION
+# dummy game for testing setup
+fastchess \
+  -engine cmd=$NEW_VERSION name=new-1 -engine cmd=$NEW_VERSION name=new-2 \
+  -each proto=uci tc=5+0.5 \
+  -rounds 1 \
+  -openings file=8moves_v3.pgn format=pgn
 
 
