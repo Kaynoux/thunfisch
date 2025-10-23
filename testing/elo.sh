ad757b3da28fecde7f253d01d7f43287ae1cb12c
heofthetea <heofthetea@gmail.com>
2025-10-23 11:58:49 +0200

    opening pgn for engine testing + rough-edged ci architecture

---
+++ testing/elo.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# requires fastchess to be installed and available through the command 'fastchess'
+
+FIRST_VERSION=
+PREVIOUS_VERSION=/tmp/thunfisch
+NEW_VERSION=../target/release/thunfisch
+
+run_sprt() {
+    # todo: name the new engine after the branch or sth
+    fastchess \
+        -engine cmd=../target/release/thunfisch name=new -engine cmd=$(PREVIOUS_VERSION) name=old \
+        -each proto=uci tc=5+0.5 \
+        -rounds 20 \
+        -repeat \
+        -openings file=8moves_v3.pgn format=pgn order=random \
+        -sprt elo0=0 elo1=5 alpha=0.05 beta=0.05
+}
+
+# dummy for testing
+echo "help" | $(NEW_VERSION)
+
+
