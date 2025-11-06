f0f22622ecc333fe30943e1f800ed323ad199ec3
heofthetea <heofthetea@gmail.com>
2025-11-06 09:40:15 +0100

    local sprt script

--- testing/sprt.sh
+++ testing/sprt.sh
@@ -1,12 +1,35 @@
 #!/bin/sh
 # requires fastchess to be installed and available through the command 'fastchess'
 
+# ensure we start at root project
+# yes this dies if you run it from somewhere else completely but fuck it
+
+ROOT_DIR=$(pwd | sed -E "s#/testing/?##g")
+
 FIRST_VERSION=
+BUILD_DIR=$ROOT_DIR/target/release/
 PREVIOUS_VERSION=./previous
 NEW_VERSION=./current
 
+build() {
+    cd $ROOT_DIR
+    branch=$(git branch | grep -P "^\* " | cut -c 3-)
+    git stash
+    git checkout master
+    git fetch && git pull
+    cargo build --locked --release
+    cp -f $BUILD_DIR/thunfisch /tmp/thunfisch-previous
+    # done building master
+    git checkout $branch
+    git stash pop
+    cargo build --locked --release
+    mv $BUILD_DIR/thunfisch ./testing/current
+    mv /tmp/thunfisch-previous ./testing/previous
+}
+
 run_sprt() {
     # todo: name the new engine after the branch or sth
+    cd $ROOT_DIR/testing
     fastchess \
         -engine cmd=$NEW_VERSION name=current -engine cmd=$PREVIOUS_VERSION name=previous \
         -each proto=uci tc=5+0.5 \
@@ -17,4 +40,5 @@ run_sprt() {
         -recover \
         -sprt elo0=0 elo1=100 alpha=0.005 beta=0.005
 }
-run_sprt
+
+build && run_sprt
