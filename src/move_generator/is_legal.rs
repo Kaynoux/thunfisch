247699b6c72b5aaa392d020a32b0d4f0457d9b8d
heofthetea <heofthetea@gmail.com>
2026-04-01 17:24:35 +0200

    lukas' test

--- src/move_generator/is_legal.rs
+++ src/move_generator/is_legal.rs
@@ -5,10 +5,20 @@ use crate::{
             KING_TARGETS, KNIGHT_TARGETS, PAWN_ATTACK_TARGETS, pawn_quiet_double_target,
             pawn_quiet_single_target,
         },
+        pinmask::generate_pin_masks,
         sliding_targets::{get_bishop_targets, get_rook_targets},
     },
     prelude::*,
 };
+
+enum MoveDirection {
+    HV,
+    Diag,
+    Knight,
+}
+impl DecodedMove {
+    fn move_direction() {}
+}
 
 impl Board {
     /// Checks whether `mv` is legal on `self`.
@@ -37,20 +47,6 @@ impl Board {
             return false;
         }
 
-        // Does our move cause there to still be a check?
-        // NOTE: doing the entire make move routine here does unnecessary calculationns (repetition stack etc)
-        // maybe we should do a stripped-down version that just applies the necessary changes to check for checks to reduce load
-        self.toggle_current_color();
-        self.make_move(mv);
-
-        let (_, next_pos_check_count) = masks::calc_check_mask(self);
-        self.unmake_move();
-        self.toggle_current_color();
-
-        if next_pos_check_count > 0 {
-            return false;
-        }
-
         // Is the piece even allowed to move there?
         let opponents = self.color_bbs_without_king(!self.current_color());
 
@@ -68,7 +64,12 @@ impl Board {
                 | MoveType::BishopPromo
                 | MoveType::RookPromo
                 | MoveType::QueenPromo => {
-                    pawn_quiet_single_target(mv.from.to_bit(), self.current_color()) == to
+                    // print!(
+                    //     "{:?}",
+                    //     pawn_quiet_single_target(mv.from.to_bit(), self.current_color())
+                    //         .to_coords()
+                    // );
+                    return pawn_quiet_single_target(mv.from.to_bit(), self.current_color()) == to;
                 }
                 MoveType::Capture
                 | MoveType::KnightPromoCapture
@@ -83,13 +84,15 @@ impl Board {
                 MoveType::EpCapture => self.ep_target().map_or(false, |target| target == to),
                 _ => false,
             },
-            Knight => KNIGHT_TARGETS[mv.from.i()].is_position_set(to),
+            Knight => {
+                (KNIGHT_TARGETS[mv.from.i()] & (opponents | self.empty())).is_position_set(to)
+            }
             Bishop => (get_bishop_targets(mv.from, self.occupied()) & (opponents | self.empty()))
                 .is_position_set(to),
             Rook => (get_rook_targets(mv.from, self.occupied()) & (opponents | self.empty()))
                 .is_position_set(to),
             Queen => ((get_rook_targets(mv.from, self.occupied()) & (opponents | self.empty()))
-                | (get_bishop_targets(mv.from, self.occupied() & (opponents | self.empty()))))
+                | (get_bishop_targets(mv.from, self.occupied()) & (opponents | self.empty())))
             .is_position_set(to),
             King => {
                 match mv.mv_type {
@@ -108,6 +111,22 @@ impl Board {
         } {
             return false;
         }
+
+        // Does our move cause there to still be a check?
+        // NOTE: doing the entire make move routine here does unnecessary calculationns (repetition stack etc)
+        // maybe we should do a stripped-down version that just applies the necessary changes to check for checks to reduce load
+        self.toggle_current_color();
+        self.make_move(mv);
+
+        let (_, next_pos_check_count) = masks::calc_check_mask(self);
+        self.unmake_move();
+        self.toggle_current_color();
+
+        if next_pos_check_count > 0 {
+            return false;
+        }
+
+        let (hv_pinmask, diag_pinmask) = generate_pin_masks(self);
 
         true
     }
@@ -172,8 +191,6 @@ impl Board {
 mod tests {
     use arrayvec::ArrayVec;
 
-    use crate::move_generator::is_legal;
-
     use super::*;
 
     #[test]
@@ -182,21 +199,15 @@ mod tests {
         let mut board = Board::from_fen(fen1);
         let legal_moves = board.generate_moves::<false>();
         for mv in legal_moves {
-            if !board.is_legal(&mv.decode()) {
-                println!("{}", mv.decode().to_coords());
-                assert!(false);
-            }
-            assert!(true)
+            // println!("{}", mv.decode().to_coords());
+            assert!(board.is_legal(&mv.decode()))
         }
         // Move the king out of check and check the rest of the clusterfuck for legality
         board.make_move(&DecodedMove::from_coords("g1h1".to_owned(), &board));
         let legal_moves = board.generate_moves::<false>();
         for mv in legal_moves {
-            if !board.is_legal(&mv.decode()) {
-                println!("{}", mv.decode().to_coords());
-                assert!(false);
-            }
-            assert!(true)
+            // println!("{}", mv.decode().to_coords());
+            assert!(board.is_legal(&mv.decode()))
         }
     }
 
@@ -205,28 +216,27 @@ mod tests {
         let fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1R1K b kq - 0 1";
         let mut board = Board::from_fen(fen);
         let legal_moves: ArrayVec<EncodedMove, 256> = board.generate_moves::<false>();
-        let legal_moves_as_coords: Vec<String> = legal_moves.iter().map(|mv| mv.decode().to_coords()).collect();
+        let legal_moves_as_coords: Vec<String> = legal_moves
+            .iter()
+            .map(|mv| mv.decode().to_coords())
+            .collect();
         let mut illegal_moves: Vec<EncodedMove> = vec![];
         for from_row in (97..=104).map(|n| (n as u8 as char).to_string()) {
             for from_col in 1..=8 {
                 for to_row in (97..=104).map(|n| (n as u8 as char).to_string()) {
                     for to_col in 1..=8 {
-                        let mv_coords =  format!("{}{}{}{}", from_row, from_col, to_row, to_col);
+                        let mv_coords = format!("{}{}{}{}", from_row, from_col, to_row, to_col);
                         if legal_moves_as_coords.contains(&mv_coords) {
                             continue;
                         }
-                        let mv = DecodedMove::from_coords(
-                            mv_coords,
-                            &board,
-                        )
-                        .encode();
+                        let mv = DecodedMove::from_coords(mv_coords, &board).encode();
                         illegal_moves.push(mv);
                     }
                 }
             }
         }
         for mv in illegal_moves {
-                println!("{}", mv.decode().to_coords());
+            // println!("{}", mv.decode().to_coords());
             // work-around to only print the information we need for debugging
             if board.is_legal(&mv.decode()) {
                 println!("{}", mv.decode().to_coords());
@@ -234,5 +244,129 @@ mod tests {
             }
             assert!(true)
         }
+    }
+}
+
+#[cfg(test)]
+mod test_lukas {
+    use crate::move_generator::is_legal;
+    use crate::move_generator::{generator::ARRAY_LENGTH, moves};
+    use crate::prelude::*;
+    use arrayvec::ArrayVec;
+
+    fn is_legal_generate(board: &mut Board) -> ArrayVec<EncodedMove, ARRAY_LENGTH> {
+        const SPECIAL_MOVES_ONLY: bool = false;
+        let friendly = board.current_color();
+        let mut moves = ArrayVec::<EncodedMove, ARRAY_LENGTH>::new();
+
+        let (hv_pinmask, diag_pinmask) = (Bitboard::EMPTY, Bitboard::EMPTY);
+        let pinmask = Bitboard::EMPTY;
+
+        let (check_mask, check_counter) = (Bitboard::FULL, 0);
+
+        moves::generate_pawn_moves::<false>(
+            &mut moves,
+            board,
+            friendly,
+            hv_pinmask,
+            diag_pinmask,
+            check_mask,
+        );
+        moves::generate_knight_moves::<SPECIAL_MOVES_ONLY>(
+            &mut moves, pinmask, friendly, board, check_mask,
+        );
+        moves::generate_bishop_moves::<SPECIAL_MOVES_ONLY>(
+            &mut moves,
+            hv_pinmask,
+            diag_pinmask,
+            friendly,
+            board,
+            check_mask,
+        );
+
+        moves::generate_rook_moves::<SPECIAL_MOVES_ONLY>(
+            &mut moves,
+            hv_pinmask,
+            diag_pinmask,
+            friendly,
+            board,
+            check_mask,
+        );
+
+        moves::generate_queen_moves::<SPECIAL_MOVES_ONLY>(
+            &mut moves,
+            hv_pinmask,
+            diag_pinmask,
+            friendly,
+            board,
+            check_mask,
+        );
+
+        moves::generate_king_move::<SPECIAL_MOVES_ONLY>(&mut moves, friendly, board);
+
+        if !SPECIAL_MOVES_ONLY {
+            moves::generate_castle_moves(&mut moves, check_counter, friendly, board);
+        }
+
+        moves::generate_ep_moves(board, &mut moves, friendly, hv_pinmask, diag_pinmask);
+
+        moves
+    }
+
+    #[test]
+    /// Tests the move generation by checking if it finds the correct amount of moves
+    /// Also tests if the hashing works by checking if the incremental hash is the same as a newly calculated one from scratch
+    fn test_is_legal() {
+        // Source: https://www.chessprogramming.org/Perft_Results
+        // Position 2 at depth 4 has all types of moves covered
+        let fens = [
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", // Initial Pos
+            "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ", // Pos 2
+            "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 ",               // Pos 3
+            "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", // Pos 4
+            "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8 ", // Pos 5
+            "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ", // Pos 6
+        ];
+        let perft_results: [[usize; 5]; 6] = [
+            [20, 400, 8_902, 197_281, 4_865_609],        // Initial Pos
+            [48, 2_039, 97_862, 4_085_603, 193_690_690], // Pos 2
+            [14, 191, 2_812, 43_238, 674_624],           // Pos 3
+            [6, 264, 9_467, 422_333, 15_833_292],        // Pos 4
+            [44, 1_486, 62_379, 2_103_487, 89_941_194],  // Pos 5
+            [46, 2_079, 89_890, 3_894_594, 164_075_551], // Pos 6
+        ];
+
+        for (fen_idx, fen) in fens.iter().enumerate() {
+            for (depth_idx, correct_node_count) in perft_results[fen_idx].iter().take(6).enumerate()
+            {
+                let mut board = Board::from_fen(fen);
+                let calculated_node_count = is_legal_r_perft(&mut board, depth_idx + 1);
+                assert_eq!(
+                    *correct_node_count,
+                    calculated_node_count,
+                    "Testing node count Fen: {} Depth: {}",
+                    fen_idx + 1,
+                    depth_idx + 1
+                );
+            }
+        }
+    }
+
+    fn is_legal_r_perft(board: &mut Board, depth: usize) -> usize {
+        if depth == 0 {
+            return 1;
+        }
+        let mut nodes = 0;
+        let moves = is_legal_generate(board);
+        assert!(moves.len() > 0);
+
+        for mv in moves {
+            if board.is_legal(&mv.decode()) {
+                let mut b2 = board.clone();
+                b2.make_move(&mv.decode());
+                nodes += is_legal_r_perft(&mut b2, depth - 1);
+            }
+        }
+        nodes
     }
 }
